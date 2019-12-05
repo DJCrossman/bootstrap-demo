32da9d9ef78510d027fefc6967af078af9bf7065
DJCrossman <david@crossman.io>
2019-12-04 20:15:39 -0600

    add pug files

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const port = process.env.PORT || 3000
 const app = express()
 
+app.set('view engine', 'pug')
 app.use(express.static('public'))
+
+app.get('', (req, res) => {
+  res.render('index', {})
+})
 
 app.listen(port, () => console.log(`http://localhost:${port}`))
