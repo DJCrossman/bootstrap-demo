a7beae42cad0c950662eb4cdf5f14e9e4be67b46
amitkumar-khatiwala <amitkumarkhatiwala@gmail.com>
2019-12-05 19:08:29 -0600

    Commit

--- index.js
+++ index.js
@@ -25,6 +25,38 @@ app.get('', (req, res) => {
     tests: summationTests,
     passed: !summationTests.some(t => !t.passed)
   })
+
+const powerEquals = (callback, ...q, a) => callback(q) === a
+
+function numberToPower(number,power)
+{
+  if (power === 0)
+    return 1;
+  var total = number;
+  for (var loop = 1; loop < power; loop ++)
+    total = total*number;
+
+  return total;
+}
+
+app.get('', (req, res) => {
+  const problems = []
+  let numberToPowerValues = [
+    { value: (3,2), answer: 9 },
+    { value: (2,3), answer: 8},
+    { value: (10,6), answer: 1000000}
+
+  ]
+
+  let numberToPowerTests = numberToPowerValues.map(t => ({...t, passed: powerEquals(numberToPower,t.value, t.answer)}))
+  problems.push({
+    name: 'Grasshopper - power',
+    method: 'numberToPower',
+    link: 'codewars.com/kata/562926c855ca9fdc4800005b',
+    tests: numberToPowerTests,
+    passed: !numberToPowerTests.some(t => !t.passed)
+  })
+
   res.render('index', {
     problems
   })
