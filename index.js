e9d4f94b315f25f6f2755fadf395d2785ec27fae
amitkumar-khatiwala <amitkumarkhatiwala@gmail.com>
2019-12-05 19:55:36 -0600

    commit

--- index.js
+++ index.js
@@ -5,25 +5,37 @@ const app = express()
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
-const summation = n => n * (n + 1) / 2;
-const assetEquals = (callback, q, a) => callback(q) === a
+// const summation = n => n * (n + 1) / 2;
+
+
+function numberToPower(number,power)
+{
+  if (power === 0)
+    return 1;
+  var total = number;
+  for (var loop = 1; loop < power; loop ++)
+    total = total*number;
+
+  return total;
+}
+const assetEquals = (callback, p,q, a) => callback(p,q) === a
 
 app.get('', (req, res) => {
   const problems = []
-  let summationValues = [
-    { value: 1, answer: 1},
-    { value: 8, answer: 36},
-    { value: 22, answer: 253},
-    { value: 100, answer: 5050},
-    { value: 213, answer: 22791}
+  let numberToPowerValues = [
+    { value1: 3,value2:2, answer: 9 },
+    { value1: 2,value2:3, answer: 8},
+    { value1: 10,value2:6, answer: 1000000}
+
   ]
-  let summationTests = summationValues.map(t => ({...t, passed: assetEquals(summation, t.value, t.answer)}))
+
+  let numberToPowerTests = numberToPowerValues.map(t => ({...t, passed: assetEquals(numberToPower,t.value1,t.value2, t.answer)}))
   problems.push({
-    name: 'Grasshopper - Summation',
-    method: 'summation',
-    link: 'https://www.codewars.com/kata/grasshopper-summation',
-    tests: summationTests,
-    passed: !summationTests.some(t => !t.passed)
+    name: 'Grasshopper - power',
+    method: 'numberToPower',
+    link: 'codewars.com/kata/562926c855ca9fdc4800005b',
+    tests: numberToPowerTests,
+    passed: !numberToPowerTests.some(t => !t.passed)
   })
   res.render('index', {
     problems
